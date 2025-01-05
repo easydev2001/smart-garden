6cf0d75267108c91c16c58384d64ab93548fe829
easydev2001 <easydev2001@gmail.com>
2025-01-05 10:48:00 +0700

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -48,7 +48,7 @@ const Home = () => {
           </div>
           <div className='flex flex-col items-center justify-center gap-4 w-40 h-40 rounded-md shadow-md bg-white'>
             <h1 className='text-xl font-medium'>Độ PH</h1>
-            <p className='text-4xl font-bold text-purple-500'>{data?.pH}</p>
+            <p className='text-4xl font-bold text-purple-500'>{(data?.pH*14/9).toFixed(0)}</p>
           </div>
         </div>
         <div className='flex flex-col flex-1 gap-4 bg-white rounded-md shadow-md'>
